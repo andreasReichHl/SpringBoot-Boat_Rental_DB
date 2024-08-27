9fdc22bfcd79ba7c78c7c5fbddbce0c6138bb705
andreashl09 <a9279352@gmail.com>
2024-08-27 14:11:57 +0200

    kickoff

---
+++ src/test/java/org/example/boat_rental/BoatRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boat_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
